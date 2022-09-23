fb8173c08d53c97b59568cd3caa6c3ad20066666
Nisa Eda Akkaya <110236429+nisaeda@users.noreply.github.com>
2022-09-23 14:54:01 +0300

    Update and rename Flatten.py to flatten.py

--- flatten.py
+++ flatten.py
@@ -1,12 +1,11 @@
 def flatten(n):
+    new_list=[]
     for i in n:
         if type(i) is list:
             flatten(i)
-
         else:
             newlist.append(i)
 
 input = [[1, 'a', ['cat'], 2], [[[3]], 'dog'], 4, 5]
-newlist = []
 flatten(input)
 print("flatten: " ,newlist)
